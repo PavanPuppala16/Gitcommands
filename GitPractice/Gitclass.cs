3a34fbabcf8d79e8df52f464a2b4ee18f7ea65e3
PavanPuppala16 <pavankumarp@fixitytech.com>
2022-12-15 10:23:18 +0530

    Testing

---
+++ GitPractice/Gitclass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPractice
+{
+  internal class Gitclass
+  {
+    public static void method1()
+    {
+      Console.WriteLine("pavan");
+    }
+  }
+}
